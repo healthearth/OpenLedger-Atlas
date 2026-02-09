c3a2aef40c6586a0f4c0b32271a4c1d49ff258ef
Repository Gang <178992666+repositorgan@users.noreply.github.com>
2026-02-09 11:33:57 -0800

    Create GlobalUsings.cs

---
+++ GlobalUsings.cs
@@ -0,0 +1,10 @@
+// GlobalUsings.cs
+global using System;
+global using System.Collections.Generic;
+global using System.Threading.Tasks;
+global using Microsoft.Data.SqlClient;
+global using Dapper;
+
+// Bridges your specific folders
+global using GlobalBank.Application.DTOs;
+global using GlobalBank.Core.Domain.Entities;
