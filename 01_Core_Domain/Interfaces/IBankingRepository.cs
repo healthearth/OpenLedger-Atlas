41677af713b4991c4edd28f727e422b9127e330a
Repository Gang <178992666+repositorgan@users.noreply.github.com>
2026-02-09 12:56:47 -0800

    Update IBankingRepository.cs

--- 01_Core_Domain/Interfaces/IBankingRepository.cs
+++ 01_Core_Domain/Interfaces/IBankingRepository.cs
@@ -1,26 +1,25 @@
 // Filepath: fintechs-exhibitu/01_Core_Domain/Interfaces/IBankingRepository.cs
 using GlobalBank.Domain.Entities;
 
-namespace GlobalBank.Domain.Interfaces
+namespace GlobalBank.Domain.Interfaces;
+
+public interface IBankingRepository
 {
-    public interface IBankingRepository
-    {
-        Task<Account?> GetAccountByIdAsync(Guid id);
+    Task<Account?> GetAccountByIdAsync(Guid id);
 
-        Task InsertLedgerEntryAsync(
-            Guid accountId,
-            decimal credit,
-            decimal debit,
-            string description,
-            string? physicalAssetRef = null
-        );
+    Task InsertLedgerEntryAsync(
+        Guid accountId,
+        decimal credit,
+        decimal debit,
+        string description,
+        string? physicalAssetRef = null
+    );
 
-        Task RegisterPhysicalAssetAsync(
-            PhysicalAssetDeposit asset,
-            Guid targetAccountId
-        );
+    Task RegisterPhysicalAssetAsync(
+        PhysicalAssetDeposit asset,
+        Guid targetAccountId
+    );
 
-        Task<decimal> GetLedgerTotalAsync();
-        Task<decimal> GetPhysicalVaultTotalAsync();
-    }
+    Task<decimal> GetLedgerTotalAsync();
+    Task<decimal> GetPhysicalVaultTotalAsync();
 }
