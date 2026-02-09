b36b79b3e500938f3cf590e324abbc77f45591a5
Repository Gang <178992666+repositorgan@users.noreply.github.com>
2026-02-09 15:59:23 -0800

    Create AuditResult.cs

---
+++ 01_Core_Domain/ValueObjects/AuditResult.cs
@@ -0,0 +1,6 @@
+// fintechs-exhibitu/01_Core_Domain/ValueObjects/AuditResult.cs
+namespace GlobalBank.Domain.ValueObjects;
+
+public record AuditResult(decimal LedgerTotal, decimal PhysicalTotal);
+
+await _db.Accounts.SumAsync(a => a.Balance);
