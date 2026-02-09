d7619cb22b5ff2c8f148c6da14ac7b02c9f9676a
Repository Gang <178992666+repositorgan@users.noreply.github.com>
2026-02-09 12:53:11 -0800

    Create AuditResult.cs

---
+++ 01_Core_Domain/Entities/AuditResult.cs
@@ -0,0 +1,9 @@
+fintechs-exhibitu/01_Core_Domain/Entities/AuditResult.cs
+namespace GlobalBank.Domain.Entities;
+
+public class AuditResult
+{
+    public bool Passed { get; set; }
+    public string Notes { get; set; } = string.Empty;
+    public DateTime TimestampUtc { get; set; }
+}
