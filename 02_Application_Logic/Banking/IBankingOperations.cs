b22b1c0f614b5a6ab1b13ce341bfac35ebc8f56b
Repository Gang <178992666+repositorgan@users.noreply.github.com>
2026-02-09 12:11:40 -0800

    Update IBankingOperations.cs
    Extended with clearer names

--- 02_Application_Logic/Banking/IBankingOperations.cs
+++ 02_Application_Logic/Banking/IBankingOperations.cs
@@ -1,10 +1,48 @@
 // -- filepath fintechs-exhibitu/02_Application_Logic/Banking/IBankingOperations.cs
-namespace GlobalBank.Domain.Interfaces
+using GlobalBank.Domain.Entities;
+using GlobalBank.Domain.Interfaces;
+
+namespace GlobalBank.Application.Banking
 {
-    public interface IBankingOperations
+    public class BankingOperations : IBankingOperations
     {
-        Task<bool> TransactAsync(Guid senderId, Guid receiverId, decimal amount);
-        Task<bool> ReconcileWithPhysicalVaultAsync();
+        private readonly IBankingRepository _repository;
+
+        public BankingOperations(IBankingRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<bool> TransactAsync(Guid senderId, Guid receiverId, decimal amount)
+        {
+            var sender = _repository.GetAccountById(senderId);
+            var receiver = _repository.GetAccountById(receiverId);
+
+            if (sender.Balance < amount)
+                return false; // AML + overdraft guard
+
+            sender.Balance -= amount;
+            receiver.Balance += amount;
+
+            var tx = new Transaction
+            {
+                Id = Guid.NewGuid(),
+                AccountId = senderId,
+                Amount = amount,
+                TimestampUtc = DateTime.UtcNow
+            };
+
+            await _repository.SaveTransactionAsync(tx);
+            await _repository.UpdateAccountAsync(sender);
+            await _repository.UpdateAccountAsync(receiver);
+
+            return true;
+        }
+
+        public Task<bool> ReconcileWithPhysicalVaultAsync()
+        {
+            // later: compare ledger totals vs physical vault totals
+            return Task.FromResult(true);
+        }
     }
 }
-
