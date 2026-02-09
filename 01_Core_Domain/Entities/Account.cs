4783ad0b694d3676669b3713c72a7111f381fd22
Repository Gang <178992666+repositorgan@users.noreply.github.com>
2026-02-09 10:09:06 -0800

    Update and rename account.cs to Account.cs

---
+++ 01_Core_Domain/Entities/Account.cs
@@ -0,0 +1,16 @@
+// Filepath: fintechs-exhibitu/01_Core_Domain/Entities/Account.cs
+namespace GlobalBank.Domain.Entities;
+
+public class Account {
+    public Guid Id { get; private set; }
+    public string OwnerName { get; private set; }
+    public string CurrencyCode { get; private set; } // "AI$"
+    public decimal DigitalBalance { get; private set; }
+    public bool IsKycVerified { get; private set; }
+
+    public Account(string owner, string currency) {
+        Id = Guid.NewGuid();
+        OwnerName = owner;
+        CurrencyCode = currency;
+    }
+}
