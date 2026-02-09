ada3339b7a8b1599c8386c8f8834e23e97091509
Repository Gang <178992666+repositorgan@users.noreply.github.com>
2026-02-09 12:44:11 -0800

    Update GlobalUsings.cs

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -1,14 +1,6 @@
 // GlobalUsings.cs
 global using System;
-global using System.Collections.Generic;
 global using System.Threading.Tasks;
-global using Microsoft.Data.SqlClient;
-global using Dapper;
 
-// Bridges your specific folders
 global using GlobalBank.Domain.Entities;
 global using GlobalBank.Domain.Interfaces;
-
-global using GlobalBank.Application.DTOs;
-
-//global using GlobalBank.Core.Domain.Entities;
