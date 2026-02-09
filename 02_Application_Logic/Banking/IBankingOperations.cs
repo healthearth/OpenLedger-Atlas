bfa41ba4fd021fb0b26bfbcf6093289824713c2e
Repository Gang <178992666+repositorgan@users.noreply.github.com>
2026-02-09 12:57:26 -0800

    Update IBankingOperations.cs

--- 02_Application_Logic/Banking/IBankingOperations.cs
+++ 02_Application_Logic/Banking/IBankingOperations.cs
@@ -1,9 +1,8 @@
 // -- filepath fintechs-exhibitu/02_Application_Logic/Banking/IBankingOperations.cs
-namespace GlobalBank.Domain.Interfaces
+namespace GlobalBank.Domain.Interfaces;
+
+public interface IBankingOperations
 {
-    public interface IBankingOperations
-    {
-        Task<bool> TransactAsync(Guid from, Guid to, decimal amount);
-        Task<bool> ReconcileWithPhysicalVaultAsync();
-    }
+    Task<bool> TransactAsync(Guid from, Guid to, decimal amount);
+    Task<bool> ReconcileWithPhysicalVaultAsync();
 }
