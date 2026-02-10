59429ae0b057bbe0c0b669c3438e392efc4a2deb
Repository Gang <178992666+repositorgan@users.noreply.github.com>
2026-02-09 17:18:55 -0800

    Update P2PTransferService.cs

--- 02_Application_Logic/Transfers/P2PTransferService.cs
+++ 02_Application_Logic/Transfers/P2PTransferService.cs
@@ -1,30 +1,34 @@
 // Filepath: fintechs-exhibitu/02_Application_Logic/Transfers/P2PTransferService.cs
+
 using GlobalBank.Domain.Interfaces;
 
 namespace GlobalBank.Application.Transfers;
 
-public class P2PTransferService {
-
-    // 1. Injected dependencies
+public class P2PTransferService
+{
     private readonly IBankingOperations _ops;
     private readonly IAmlService _aml;
 
-    // 2. The private constructor - injects the dependencies here
     public P2PTransferService(IBankingOperations ops, IAmlService aml)
     {
         _ops = ops ?? throw new ArgumentNullException(nameof(ops));
         _aml = aml ?? throw new ArgumentNullException(nameof(aml));
-
     }
 
-    // 3. Methods
     public async Task<bool> TransferAsync(Guid from, Guid to, decimal amount)
     {
+        // Basic validation
+        if (amount <= 0)
+            return false;
+
+        if (from == to)
+            return false;
+
         // AML check
         if (!await _aml.IsTransferAllowedAsync(from, to, amount))
             return false;
 
-        // Banking operation, "the handshake"
+        // Execute the transfer
         return await _ops.TransactAsync(from, to, amount);
     }
 }
