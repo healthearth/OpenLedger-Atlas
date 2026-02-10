d8086cb9d02861324a40ff45c21fad7b0088d1b8
Repository Gang <178992666+repositorgan@users.noreply.github.com>
2026-02-09 17:46:58 -0800

    Update CapitalRegistrationService.cs

--- 02_Application_Logic/Services/CapitalRegistrationService.cs
+++ 02_Application_Logic/Services/CapitalRegistrationService.cs
@@ -2,6 +2,7 @@
 
 using GlobalBank.Domain.Interfaces;
 using GlobalBank.Domain.Entities;
+using GlobalBank.Domain.Services;
 
 namespace GlobalBank.Application.Services;
 
@@ -21,9 +22,9 @@ public class CapitalRegistrationService
         foreach (var asset in assets)
         {
             // 1. Convert physical currency to AI$ equivalent
-            decimal aiValue = ExchangeEngine.ConvertToAiDollar(
+            decimal aiValue = ExchangeEngine.Convert(asset.FaceValue, asset.CurrencyCode);
                 asset.FaceValue,
-                asset.CurrencyCode);
+                asset.CurrencyCode;
 
             // 2. Register the physical asset in the vault
             await _repo.RegisterPhysicalAssetAsync(asset, targetAccountId);
