149bba802218947455b1530c2a005c9d32b1ff9a
Repository Gang <178992666+repositorgan@users.noreply.github.com>
2026-02-09 16:14:44 -0800

    Update P2PTransferService.cs
    ) and a spacing error resolved

--- 02_Application_Logic/Transfers/P2PTransferService.cs
+++ 02_Application_Logic/Transfers/P2PTransferService.cs
@@ -17,12 +17,13 @@ public class P2PTransferService {
     }
 
     // 3. Methods
-    public async Task<bool> TransferAsync(Guidfrom, Guid to, decimal amount)
+    public async Task<bool> TransferAsync(Guid from, Guid to, decimal amount)
     {
         // AML check
         if (!await _aml.IsTransferAllowedAsync(from, to, amount))
+            return false;
 
         // Banking operation, "the handshake"
-        return await _ops.TransactAsync(from, to, amount));
+        return await _ops.TransactAsync(from, to, amount);
     }
 }
