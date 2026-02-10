6308ea9671e558c7944f2a49c13860f2700f0fb0
Repository Gang <178992666+repositorgan@users.noreply.github.com>
2026-02-09 16:37:18 -0800

    Update AuditResult.cs
    updated decimal with bool

--- 01_Core_Domain/ValueObjects/AuditResult.cs
+++ 01_Core_Domain/ValueObjects/AuditResult.cs
@@ -1,7 +1,7 @@
 // fintechs-exhibitu/01_Core_Domain/ValueObjects/AuditResult.cs
 namespace GlobalBank.Domain.ValueObjects;
 
-public record AuditResult(decimal LedgerTotal, decimal PhysicalTotal);
+public record AuditResult(bool IsCompliant, decimal Discrepancy);
 
 // await _db.Accounts.SumAsync(a => a.Balance);
 
