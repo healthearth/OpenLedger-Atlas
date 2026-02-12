8ce9208a4abf5d89becea54318d18f412b4e6a57
Health 3 the Earth <1akieckhefer@gmail.com>
2026-02-11 22:59:53 -0800

    Update Money.cs
    Created factory workers to simplify transcription of currency value.

--- 01_Core_Domain/ValueObjects/Money.cs
+++ 01_Core_Domain/ValueObjects/Money.cs
@@ -1,6 +1,7 @@
 // Filepath: fintechs-exhibitu/01_Core_Domain/ValueObjects/Money.cs
 // © 2026 Andrew Kieckhefer. All rights reserved.
 
+// Establish the presence of value 
 public record Money {
     public decimal Amount { get; init; }
     public string Currency { get; init; } // e.g. "AI$"
@@ -10,4 +11,18 @@ public record Money {
         Amount = Math.Round(amount, 2); // Regulatory standard precision
         Currency = currency;
     }
+
+    // Ergonomic Factory Helpers
+    public static Money Osb(decimal amount) => 
+        new Money(amount, Currency.Osb.Code); 
+    public static Money Btc(decimal amount) => 
+        new Money(amount, Currency.Bitcoin.Code); 
+    public static Money Eth(decimal amount) => 
+        new Money(amount, Currency.Ethereum.Code); 
+    public static Money Vnd(decimal amount) => 
+        new Money(amount, Currency.VietnameseDong.Code); 
+    public static Money Crc(decimal amount) => 
+        new Money(amount, Currency.CostaRicanColon.Code); 
+    public static Money Usd(decimal amount) => 
+        new Money(amount, Currency.Usd.Code);
 }
