3ffa60cbcd31f0a397710e8578bce4921ed468a9
Health 3 the Earth <1akieckhefer@gmail.com>
2026-02-14 12:54:59 -0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
-// fintechs-exhibitu/Program.cs
+if (args.Contains("--simulate"))
+{
+    using var scope = app.Services.CreateScope();
+    var simulator = scope.ServiceProvider.GetRequiredService<TransactionSimulator>();
+
+    await simulator.GenerateAsync(10000);
+
+    Console.WriteLine("Generated 10,000 transactions.");
+    return;
+}
