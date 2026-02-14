84517e0924db5ea03d3a5772a4d61fac53c4a262
Health 3 the Earth <1akieckhefer@gmail.com>
2026-02-14 04:29:09 -0800

    Update OpenLedgerDbContext.cs

--- 03_Infrastructure/Data/OpenLedgerDbContext.cs
+++ 03_Infrastructure/Data/OpenLedgerDbContext.cs
@@ -1,1 +1,10 @@
 //
+public class OpenLedgerDbContext : DbContext
+{
+    public OpenLedgerDbContext(DbContextOptions<OpenLedgerDbContext> options)
+        : base(options) { }
+
+    public DbSet<User> Users => Set<User>();
+    public DbSet<Account> Accounts => Set<Account>();
+    public DbSet<Transaction> Transactions => Set<Transaction>();
+}
