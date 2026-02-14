3175718b441b084f068bc8c740c7d3c94fba3e1e
Health 3 the Earth <1akieckhefer@gmail.com>
2026-02-14 04:34:20 -0800

    Update TransferService.cs

--- 02_Application_Logic/Services/TransferService.cs
+++ 02_Application_Logic/Services/TransferService.cs
@@ -1,1 +1,32 @@
 //
+public class TransferService
+{
+    private readonly OpenLedgerDbContext _db;
+
+    public TransferService(OpenLedgerDbContext db)
+    {
+        _db = db;
+    }
+
+    public async Task<bool> Transfer(Guid fromUserId, Guid toUserId, decimal amount)
+    {
+        var from = await _db.Accounts.FirstAsync(a => a.UserId == fromUserId);
+        var to = await _db.Accounts.FirstAsync(a => a.UserId == toUserId);
+
+        if (from.Balance < amount)
+            return false;
+
+        from.Balance -= amount;
+        to.Balance += amount;
+
+        _db.Transactions.Add(new Transaction
+        {
+            FromAccountId = from.Id,
+            ToAccountId = to.Id,
+            Amount = amount
+        });
+
+        await _db.SaveChangesAsync();
+        return true;
+    }
+}
