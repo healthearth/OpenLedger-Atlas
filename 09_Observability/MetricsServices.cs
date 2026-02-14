25e9e897489bea0672f3bd2991c38b446cb6524c
Health 3 the Earth <1akieckhefer@gmail.com>
2026-02-14 04:34:45 -0800

    Update MetricsServices.cs

--- 09_Observability/MetricsServices.cs
+++ 09_Observability/MetricsServices.cs
@@ -1,1 +1,24 @@
 //
+public class MetricsService
+{
+    private readonly OpenLedgerDbContext _db;
+
+    public MetricsService(OpenLedgerDbContext db)
+    {
+        _db = db;
+    }
+
+    public async Task<object> GetMetrics()
+    {
+        var totalUsers = await _db.Users.CountAsync();
+        var totalTx = await _db.Transactions.CountAsync();
+        var totalVolume = await _db.Transactions.SumAsync(t => t.Amount);
+
+        return new
+        {
+            Users = totalUsers,
+            Transactions = totalTx,
+            Volume = totalVolume
+        };
+    }
+}
