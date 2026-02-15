d217d8da3cc47212438e13e109e4e59d86e1ea93
Health 3 the Earth <1akieckhefer@gmail.com>
2026-02-15 08:24:04 -0800

    Update OpenLedgerDbContext.cs

--- 03_Infrastructure/Data/OpenLedgerDbContext.cs
+++ 03_Infrastructure/Data/OpenLedgerDbContext.cs
@@ -1,10 +1,14 @@
-//
-public class OpenLedgerDbContext : DbContext
-{
-    public OpenLedgerDbContext(DbContextOptions<OpenLedgerDbContext> options)
-        : base(options) { }
+using Microsoft.EntityFrameworkCore;
 
-    public DbSet<User> Users => Set<User>();
-    public DbSet<Account> Accounts => Set<Account>();
-    public DbSet<Transaction> Transactions => Set<Transaction>();
+namespace OpenSourceBank.Infrastructure.Data
+{
+    public class OpenLedgerDbContext : DbContext
+    {
+        public OpenLedgerDbContext(DbContextOptions<OpenLedgerDbContext> options)
+            : base(options) { }
+
+        public DbSet<User> Users => Set<User>();
+        public DbSet<Account> Accounts => Set<Account>();
+        public DbSet<Transaction> Transactions => Set<Transaction>();
+    }
 }
