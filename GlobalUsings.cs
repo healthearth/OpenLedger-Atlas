2732e0f899ea95b4eb17edd274ac3af1fe962e8a
Health 3 the Earth <1akieckhefer@gmail.com>
2026-02-15 13:27:01 -0800

    Update GlobalUsings.cs

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -6,3 +6,6 @@ global using System.Threading.Tasks;
 
 global using OpenLedgerAtlas.Domain.Entities;
 global using OpenLedgerAtlas.Domain.Interfaces;
+global using OpenLedgerAtlas.Domain.Services; 
+global using OpenLedgerAtlas.Infrastructure.Data; 
+global using Microsoft.EntityFrameworkCore;
