83960033b8bff2783ebd6381aa918e6ac683288d
Health 3 the Earth <1akieckhefer@gmail.com>
2026-02-15 16:45:51 -0800

    Update Program.cs
    bypass calling an int

--- Program.cs
+++ Program.cs
@@ -41,7 +41,7 @@ if (args.Contains("--simulate"))
     using var scope = app.Services.CreateScope();
     var simulator = scope.ServiceProvider.GetRequiredService<TransactionSimulator>();
 
-    await simulator.GenerateAsync(10000);
+    await simulator.GenerateAsync(new List <Guid>(), 10000);
     Console.WriteLine("Generated 10,000 transactions.");
     return;
 }
